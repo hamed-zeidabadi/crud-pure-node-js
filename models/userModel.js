33857411fdb75bd65d4dbefae45819b34bfe4ff7
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-09-30 12:05:27 +0330

    craete model user

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const userList = require('../data/users.json');
+const { writeData } = require('../fs');
+
+
+const findAllUser = () => {
+    return new Promise((resolve, reject) => {
+        resolve(userList);
+    })
+}
+
+const findUserByID = (id) => {
+    return new Promise((resolve, reject) => {
+        const user = userList.find((p) => p.id === id);
+        resolve(user);
+    })
+}
+
+const createUser = (user) => {
+    return new Promise((resolve, reject) => {
+        const newUser = { id: String(userList.length + 1), ...user }
+        UserList.push(newUser)
+        writeData('./data/user.json', userList);
+        resolve(newUser)
+    })
+}
+
+
+
+module.exports = {
+    findAllUser: findAll, findUserByID: find, createUser: create
+}
