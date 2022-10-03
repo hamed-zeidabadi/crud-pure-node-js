c1bd1909dd7ba192464bae90557df929552993e3
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-10-03 19:38:38 +0330

    last retouch models

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,47 +1,52 @@
-const todoList = require('../data/todos.json');
-const { writeData } = require('../fs');
+const todoList = require("../data/todos.json");
+const { writeData } = require("../fs");
 
-
+//get all todo
 const findAll = () => {
-    return new Promise((resolve, reject) => {
-        resolve(todoList);
-    })
-}
-
+  return new Promise((resolve, reject) => {
+    resolve(todoList);
+  });
+};
+//find todo by id
 const findByUserID = (id) => {
-    return new Promise((resolve, reject) => {
-        const todo = todoList.find((p) => p.user_id === id);
-        resolve(todo);
-    })
-}
+  return new Promise((resolve, reject) => {
+    const todo = todoList.find((p) => p.user_id === id);
+    resolve(todo);
+  });
+};
 
+//create new todo
 const create = (todo) => {
-    return new Promise((resolve, reject) => {
-        const newTodo = { id: String(todoList.length + 1), ...todo }
-        todoList.push(newTodo)
-        writeData('./data/todos.json', todoList);
-        resolve(newTodo)
-    })
-}
-
+  return new Promise((resolve, reject) => {
+    const newTodo = { id: String(todoList.length + 1), ...todo };
+    todoList.push(newTodo);
+    writeData("./data/todos.json", todoList);
+    resolve(newTodo);
+  });
+};
+//update todos by id
 const update = (id, todo) => {
-    return new Promise((resolve, reject) => {
-        const idx = todoList.findIndex((p) => p.id === id);
-        todoList[idx] = { id, ...todo }
-        writeData('./data/todos.json', todoList);
-        resolve(todoList[idx])
-    })
-}
+  return new Promise((resolve, reject) => {
+    const idx = todoList.findIndex((p) => p.id === id);
+    todoList[idx] = { id, ...todo };
+    writeData("./data/todos.json", todoList);
+    resolve(todoList[idx]);
+  });
+};
 
+//remove todo by id
 const remove = (id) => {
-    return new Promise((resolve, reject) => {
-        const newTodoList = todoList.filter((p) => p.id !== id);
-        writeData('./data/todos.json', newTodoList);
-        resolve()
-    })
-}
-
+  return new Promise((resolve, reject) => {
+    const newTodoList = todoList.filter((p) => p.id !== id);
+    writeData("./data/todos.json", newTodoList);
+    resolve();
+  });
+};
 
 module.exports = {
-    findAll, findByUserID, create, update, remove
-}
+  findAll,
+  findByUserID,
+  create,
+  update,
+  remove,
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,37 +1,41 @@
-const userList = require('../data/users.json');
-const { writeData } = require('../fs');
+const userList = require("../data/users.json");
+const { writeData } = require("../fs");
 
-
+//get all user
 const findAllUser = () => {
-    return new Promise((resolve, reject) => {
-        resolve(userList);
-    })
-}
-
+  return new Promise((resolve, reject) => {
+    resolve(userList);
+  });
+};
+//find user by username
 const findUser = (username) => {
-    return new Promise((resolve, reject) => {
-        const user = userList.find((p) => p.username === username);
-        resolve(user);
-    })
-}
+  return new Promise((resolve, reject) => {
+    const user = userList.find((p) => p.username === username);
+    resolve(user);
+  });
+};
+
+//find user by id
 const findUserByID = (id) => {
-    return new Promise((resolve, reject) => {
-        const user = userList.find((p) => p.id === id);
-        resolve(user);
-    })
-}
+  return new Promise((resolve, reject) => {
+    const user = userList.find((p) => p.id === id);
+    resolve(user);
+  });
+};
 
+//create new user
 const createUser = (user) => {
-    return new Promise((resolve, reject) => {
-        const newUser = { id: String(userList.length + 1), ...user }
-        userList.push(newUser)
-        writeData('./data/users.json', userList);
-        resolve(newUser)
-    })
-}
-
-
+  return new Promise((resolve, reject) => {
+    const newUser = { id: String(userList.length + 1), ...user };
+    userList.push(newUser);
+    writeData("./data/users.json", userList);
+    resolve(newUser);
+  });
+};
 
 module.exports = {
-    findAll: findAllUser, find: findUserByID, findUser: findUser, create: createUser
-}
+  findAll: findAllUser,
+  find: findUserByID,
+  findUser: findUser,
+  create: createUser,
+};
