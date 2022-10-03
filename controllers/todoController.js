0324bacbfdddab9e91b388ba5638b11d669a5225
hamed-zeidabadi <zeidabadi73@gmail.com>
2022-10-03 19:49:06 +0330

    last retouch controllers

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,104 +1,102 @@
-const Todo = require('./../models/todoModel');
+const Todo = require("./../models/todoModel");
+
+// get all todo =>  api/todo : @GET
 const getAllTodo = async (req, res) => {
-    try {
-        const allTodo = await Todo.findAll()
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(allTodo))
-    } catch (error) {
-        console.log(error);
-    }
-}
-
+  try {
+    const allTodo = await Todo.findAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(allTodo));
+  } catch (error) {
+    console.log(error);
+  }
+};
+// get todo by id => api/todo/id  : @GET
 const getTodoByUserId = async (req, res, id) => {
-    try {
-        const todo = await Todo.findByUserID(id);
-        if (!todo) {
-            res.writeHead(404, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ message: 'Todo Not Found !' }))
-        } else {
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(todo))
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const todo = await Todo.findByUserID(id);
+    if (!todo) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Todo Not Found !" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(todo));
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
+// create new todo => api/todo/  : @POST
 const createTodo = async (req, res) => {
-    try {
-        let body = '';
-        req.on('data', (c) => {
-            body += c.toString();
-        })
+  try {
+    let body = "";
+    req.on("data", (c) => {
+      body += c.toString();
+    });
 
-        req.on('end', async () => {
-            const { title, todo, isActive } = JSON.parse(body)
-            const newTodo = {
-                title,
-                todo,
-                isActive
-            }
-            const createTodo = await Todo.create(newTodo)
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(createTodo))
-        })
-
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-
+    req.on("end", async () => {
+      const { title, todo, isActive } = JSON.parse(body);
+      const newTodo = {
+        title,
+        todo,
+        isActive,
+      };
+      const createTodo = await Todo.create(newTodo);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(createTodo));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+// update todo =>  api/todo/ : @PATCH
 const updateTodoById = async (req, res, id) => {
-    try {
-        const todoItems = await Todo.findByID(id);
-        if (!todoItems) {
-            res.writeHead(404, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ message: 'Todo Not Found !' }))
-        } else {
-
-            let body = '';
-            req.on('data', (c) => {
-                body += c.toString();
-            });
-            req.on('end', async () => {
-                const { title, todo, isActive } = JSON.parse(body);
-                const newTodo = {
-                    title: title || todoItems.title,
-                    todo: todo || todoItems.todo,
-                    isActive: isActive || todoItems.isActive
-                };
-                const updateTodo = await Todo.update(id, newTodo);
-                await res.writeHead(201, { 'Content-Type': 'application/json' });
-                await res.end(JSON.stringify(updateTodo));
-            })
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const todoItems = await Todo.findByID(id);
+    if (!todoItems) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Todo Not Found !" }));
+    } else {
+      let body = "";
+      req.on("data", (c) => {
+        body += c.toString();
+      });
+      req.on("end", async () => {
+        const { title, todo, isActive } = JSON.parse(body);
+        const newTodo = {
+          title: title || todoItems.title,
+          todo: todo || todoItems.todo,
+          isActive: isActive || todoItems.isActive,
+        };
+        const updateTodo = await Todo.update(id, newTodo);
+        await res.writeHead(201, { "Content-Type": "application/json" });
+        await res.end(JSON.stringify(updateTodo));
+      });
     }
-}
-
+  } catch (error) {
+    console.log(error);
+  }
+};
+// delete todo =>  api/todo/ : @DELETE
 const deleteTodoById = async (req, res, id) => {
-    try {
-        const todoItems = await Todo.findByID(id);
-        if (!todoItems) {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Todo Not Found !' }));
-        } else {
-            await Todo.remove(id);
-            await res.writeHead(201, { 'Content-Type': 'application/json' });
-            await res.end(JSON.stringify({ message: `${id} removed !` }));
-        }
-    } catch (error) {
-        console.log(error);
-    };
-}
+  try {
+    const todoItems = await Todo.findByID(id);
+    if (!todoItems) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Todo Not Found !" }));
+    } else {
+      await Todo.remove(id);
+      await res.writeHead(201, { "Content-Type": "application/json" });
+      await res.end(JSON.stringify({ message: `${id} removed !` }));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    getAllTodo,
-    getTodoByUserId,
-    createTodo,
-    updateTodoById,
-    deleteTodoById
-}
+  getAllTodo,
+  getTodoByUserId,
+  createTodo,
+  updateTodoById,
+  deleteTodoById,
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,107 +1,112 @@
-const User = require('./../models/userModel');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const User = require("./../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// get all users =>  api/user/ : @GET
 const getAllUser = async (req, res) => {
-    try {
-        const allUser = await User.findAll()
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(allUser))
-    } catch (error) {
-        console.log(error);
-    }
-}
-
+  try {
+    const allUser = await User.findAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(allUser));
+  } catch (error) {
+    console.log(error);
+  }
+};
+// get user by ID =>  api/user/id : @GET
 const getUserById = async (req, res, id) => {
-    try {
-        const user = await User.findByID(id);
-        if (!user) {
-            res.writeHead(404, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ message: 'User Not Found !' }))
-        } else {
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(user))
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const user = await User.findByID(id);
+    if (!user) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "User Not Found !" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(user));
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
+// create new user =>  api/user/ : @POST
 const createUser = async (req, res) => {
-    try {
-        let body = '';
-        req.on('data', (c) => {
-            body += c.toString();
-        })
-
-        req.on('end', async () => {
-            const { username, password } = JSON.parse(body)
-            const user = await User.findUser(username);
-            if (user) {
-                res.writeHead(401, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify({ message: 'Username Already Exists' }))
-            } else if (username.length < 6) {
-                res.writeHead(401, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify({ message: 'Username Must Be More Than 6 Words' }))
-            } else if (password.length < 6) {
-                res.writeHead(401, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify({ message: 'Password Must Be More Than 6 Words' }))
-            } else {
-                const safeUser = String(username.trim());
-                const safePass = String(password.trim())
-                const hashPassword = bcrypt.hashSync(safePass, 10);
-                const newUser = { username: safeUser, password: hashPassword }
-                const createUser = await User.create(newUser)
-                res.writeHead(200, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify(createUser))
-            }
-        })
-
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
+  try {
+    let body = "";
+    req.on("data", (c) => {
+      body += c.toString();
+    });
+    req.on("end", async () => {
+      const { username, password } = JSON.parse(body);
+      const user = await User.findUser(username);
+      if (user) {
+        res.writeHead(401, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Username Already Exists" }));
+      } else if (username.length < 6) {
+        res.writeHead(401, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({ message: "Username Must Be More Than 6 Words" })
+        );
+      } else if (password.length < 6) {
+        res.writeHead(401, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({ message: "Password Must Be More Than 6 Words" })
+        );
+      } else {
+        // validator
+        const safeUser = String(username.trim());
+        const safePass = String(password.trim());
+        //Convert password to hashed password
+        const hashPassword = bcrypt.hashSync(safePass, 10);
+        const newUser = { username: safeUser, password: hashPassword };
+        const createUser = await User.create(newUser);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(createUser));
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+// handle login user with JWT  =>  api/login : @POST
 const loginUser = async (req, res) => {
-    try {
-        let body = '';
-        req.on('data', (c) => {
-            body += c.toString();
-        })
+  try {
+    let body = "";
+    req.on("data", (c) => {
+      body += c.toString();
+    });
 
-        req.on('end', async () => {
-            const { username, password } = JSON.parse(body)
-            const user = await User.findUser(username);
-            if (!user) {
-                res.writeHead(401, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify({ message: 'User Not Found !' }))
-            } else {
-                const isValid = await bcrypt.compare(password, user.password);
-                console.log('isVal:', isValid);
-                if (isValid) {
-                    const JWT_SECRET = "Hamed@123456789#"
-                    const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET);
-                    res.writeHead(200, { 'Content-Type': 'application/json' })
-                    res.end(JSON.stringify(token));
-                } else {
-                    res.writeHead(401, { 'Content-Type': 'application/json' })
-                    res.end(JSON.stringify({ message: 'Username or Password is Incorrect' }))
-                }
-
-            }
-
-        })
-
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
+    req.on("end", async () => {
+      const { username, password } = JSON.parse(body);
+      const user = await User.findUser(username);
+      if (!user) {
+        res.writeHead(401, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "User Not Found !" }));
+      } else {
+        const isValid = await bcrypt.compare(password, user.password);
+        console.log("isVal:", isValid);
+        if (isValid) {
+          //create token
+          const JWT_SECRET = "Hamed@123456789#";
+          const token = jwt.sign(
+            { id: user.id, username: user.username },
+            JWT_SECRET
+          );
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(token));
+        } else {
+          res.writeHead(401, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({ message: "Username or Password is Incorrect" })
+          );
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    getAllUser,
-    getUserById,
-    createUser,
-    loginUser
-}
+  getAllUser,
+  getUserById,
+  createUser,
+  loginUser,
+};
